e5e0249bdfdb50c1007e760fd8ac76338cbfd50d
dinesh kumar <chayaldinesh1@gmail.com>
2023-11-14 10:39:57 +0530

    formatted

--- AdminProfile/src/main/java/com/niit/AdminProfile/controller/AddToCartController.java
+++ AdminProfile/src/main/java/com/niit/AdminProfile/controller/AddToCartController.java
@@ -1,7 +1,6 @@
 package com.niit.AdminProfile.controller;
 
-import com.niit.AdminProfile.exception.DataPresentInAddToFavourite;
-import com.niit.AdminProfile.model.AddFavourite;
+
 import com.niit.AdminProfile.model.AddToCart;
 import com.niit.AdminProfile.service.AddToCartService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,25 +10,22 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/addToCart")
-public class AddToCartController
-{
+public class AddToCartController {
     @Autowired
     AddToCartService addToCartService;
 
     @PostMapping("/post")
-    ResponseEntity<?> addToCart(@RequestBody AddToCart addToCart) throws Exception
-    {
+    ResponseEntity<?> addToCart(@RequestBody AddToCart addToCart) throws Exception {
         return new ResponseEntity<>(addToCartService.addToCart(addToCart), HttpStatus.CREATED);
     }
+
     @GetMapping("/get/{email}")
-    ResponseEntity<?> getAllCart(@PathVariable String email)
-    {
-        return new ResponseEntity<>(addToCartService.getAll(email),HttpStatus.OK);
+    ResponseEntity<?> getAllCart(@PathVariable String email) {
+        return new ResponseEntity<>(addToCartService.getAll(email), HttpStatus.OK);
     }
 
     @DeleteMapping("/delete/cart/{uemailid}/{emailid}/{dishName}")
-    ResponseEntity<?> deletefromcart(@PathVariable String uemailid,@PathVariable String emailid,@PathVariable String dishName)
-    {
-        return new ResponseEntity<>(addToCartService.deleteToCart(uemailid,emailid,dishName),HttpStatus.OK);
+    ResponseEntity<?> deletefromcart(@PathVariable String uemailid, @PathVariable String emailid, @PathVariable String dishName) {
+        return new ResponseEntity<>(addToCartService.deleteToCart(uemailid, emailid, dishName), HttpStatus.OK);
     }
 }
